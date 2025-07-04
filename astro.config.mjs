f724f42e0c2d0e50ca0d4b9f0ccca709250a066f
David Pérez <taborlindp@gmail.com>
2025-07-04 17:32:52 +0200

    Agrega integración de Sanity y actualiza dependencias en package.json y package-lock.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import 'dotenv/config'
+
+import sanity from '@sanity/astro';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sanity({
+    projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+    dataset: "production",
+    useCdn: false
+  })],
 });
